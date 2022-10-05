fbf9aa98ce3b0f9177a7acf20b12f12bd4be54cb
perlmathi <63944423+perlmathi@users.noreply.github.com>
2022-10-05 12:37:45 +0530

    Updated Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
    public class Main {
         public static void main(String[] args) {
-            //code will go in here
+            System.out.println(checkIfInputIsAnEvenNumber(122)); // Testing in the main method
         }
-    }
-
-    public static boolean checkIfInputIsAnEvenNumber(int number){
-        return number % 2 == 0;
+    
+        public static boolean checkIfInputIsAnEvenNumber(int number){
+            return number % 2 == 0;
+        }
     }
